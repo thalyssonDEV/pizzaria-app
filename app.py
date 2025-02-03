5480bb3a16946b86f5e605edeb24c6cdb6960c7c
thalyssonDEV <contato.thalyssondel@gmail.com>
2025-02-03 11:57:18 -0300

    .

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from conn import connect_to_db
 
+
 # Inicializa o servidor Flask
 app = Flask(__name__)
+
 
 @app.route('/')
 def home() -> None:
     # Renderiza a página principal localmente no Flask
     return render_template('main_page.html')
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register() -> None:
     # Rota para registro de usuários (ainda não implementada)
     pass
 
+
 if __name__ == '__main__':
     # Executa o servidor Flask em modo de depuração
-    app.run(debug=True)
+    app.run(debug=False)
