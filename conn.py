042549aa410ca6588edc9aad5efdb97e22d74aa2
thalyssonDEV <contato.thalyssondel@gmail.com>
2025-02-04 11:31:02 -0300

    add: env var

--- conn.py
+++ conn.py
@@ -1,33 +1,45 @@
 import psycopg2
-import json
 import os, sys
 
-def load_db_config():
-    # Define o caminho do arquivo de configuração JSON do banco de dados
-    config_path = os.path.join(os.path.dirname(__file__), "json/db_config.json")
+# def load_db_config():
+#     # Define o caminho do arquivo de configuração JSON do banco de dados
+#     config_path = os.path.join(os.path.dirname(__file__), "json/db_config.json")
 
-    try:
-        # Abre e carrega as configurações do banco de dados a partir do arquivo JSON
-        with open(config_path, "r") as config_file:
-            return json.load(config_file)
+#     try:
+#         # Abre e carrega as configurações do banco de dados a partir do arquivo JSON
+#         with open(config_path, "r") as config_file:
+#             return json.load(config_file)
         
-    except (FileNotFoundError, json.JSONDecodeError) as e:
-        # Exibe erro caso o arquivo não seja encontrado ou contenha erro de formatação
-        print(f"Erro ao Carregar o Arquivo de Configuração: {e}")
-        sys.exit(1)  # Encerra a execução do programa
+#     except (FileNotFoundError, json.JSONDecodeError) as e:
+#         # Exibe erro caso o arquivo não seja encontrado ou contenha erro de formatação
+#         print(f"Erro ao Carregar o Arquivo de Configuração: {e}")
+#         sys.exit(1)  # Encerra a execução do programa
+
+def check_env_vars():
+    # Verifica se todas as variáveis de ambiente necessárias estão definidas
+    required_vars = ["host", "database", "user", "password", "port"]
+    missing_vars = [var for var in required_vars if not os.getenv(var)]
+    
+    if missing_vars:
+        print(f"Erro: Variáveis de ambiente ausentes: {', '.join(missing_vars)}")
+        sys.exit(1)
+
 
 def connect_to_db():
+    # Conecta ao banco de dados usando variáveis de ambiente
+    check_env_vars()
+    
     # Carrega as configurações do banco de dados
-    db_config = load_db_config()
+    # db_config = load_db_config()
 
     try:
         # Estabelece a conexão com o banco de dados utilizando os parâmetros carregados
         conn = psycopg2.connect(
-            host = db_config["host"],
-            database = db_config["database"],
-            user = db_config["user"],
-            password = db_config["password"],
-            port = db_config["port"]
+            host = os.getenv("host", "your_localhost"),              # Endereço do banco de dados
+            database = os.getenv("database", "your_database"),     # Nome do banco de dados
+            user = os.getenv("user", "your_user"),                # Usuário do banco de dados
+            password = os.getenv("password", "your_password"),     # Senha do banco de dados
+            port = os.getenv("port", "your_port")                   # Porta do banco de dados
         )
         print("Conexão Bem-Sucedida ao Banco de Dados")
 
